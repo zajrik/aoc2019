1121e971e1199d14716cec098e088f6ec4b6c25b
Zack Campbell <zajrik@gmail.com>
2019-12-26 13:51:02 -0600

    Finish day 03 part 1

--- day03/src/part1.rs
+++ day03/src/part1.rs
@@ -1,4 +1,5 @@
-#[derive(Debug, PartialEq)]
+use std::collections::HashSet;
+
 enum Direction {
     Up,
     Down,
@@ -6,7 +7,6 @@ enum Direction {
     Right,
 }
 
-#[derive(Debug, PartialEq)]
 struct WireSegment {
     direction: Direction,
     length: u32,
@@ -34,12 +34,60 @@ impl WireSegment {
     }
 }
 
-// struct Coord {
-//     x: i32,
-//     y: i32,
-// }
+#[derive(PartialEq, Eq, Hash)]
+struct Point {
+    x: i32,
+    y: i32,
+}
 
-pub fn part1() {
+impl Point {
+    const ORIGIN: Point = Point { x: 0, y: 0 };
+
+    fn new(x: i32, y: i32) -> Point {
+        Point { x, y }
+    }
+}
+
+/// Iterate over the given vector of WireSegments and return a HashSet containing
+/// all points traversed by each segment, excluding the origin
+fn process_wire_segments(segments: Vec<WireSegment>) -> HashSet<Point> {
+    let mut points: HashSet<Point> = HashSet::new();
+    let mut start: Point = Point::ORIGIN;
+
+    for segment in segments.iter() {
+        match segment.direction {
+            Direction::Up => {
+                for n in start.x..=(start.x + segment.length as i32) {
+                    points.insert(Point::new(n, start.y));
+                }
+                start = Point::new(start.x + segment.length as i32, start.y);
+            }
+            Direction::Down => {
+                for n in (start.x - segment.length as i32)..=start.x {
+                    points.insert(Point::new(n, start.y));
+                }
+                start = Point::new(start.x - segment.length as i32, start.y);
+            }
+            Direction::Right => {
+                for n in start.y..=(start.y + segment.length as i32) {
+                    points.insert(Point::new(start.x, n));
+                }
+                start = Point::new(start.x, start.y + segment.length as i32);
+            }
+            Direction::Left => {
+                for n in (start.y - segment.length as i32)..=start.y {
+                    points.insert(Point::new(start.x, n));
+                }
+                start = Point::new(start.x, start.y - segment.length as i32);
+            }
+        }
+    }
+
+    points.remove(&Point::ORIGIN);
+    points
+}
+
+pub fn part1() -> Option<()> {
     let mut input: Vec<Vec<WireSegment>> = include_str!("./input.txt")
         .replace("\r\n", "\n")
         .split("\n")
@@ -50,13 +98,14 @@ pub fn part1() {
         })
         .collect();
 
-    let wire2: Vec<WireSegment> = input.pop().unwrap();
-	let wire1: Vec<WireSegment> = input.pop().unwrap();
-	
-	let test: bool = wire1.contains(&WireSegment { direction: Direction::Right, length: 352 });
+    let wire_1: HashSet<Point> = process_wire_segments(input.pop()?);
+    let wire_2: HashSet<Point> = process_wire_segments(input.pop()?);
 
-	println!("{}", test);
+    let closest_to_origin: i32 = wire_1
+        .intersection(&wire_2)
+        .map(|p| p.x.abs() + p.y.abs())
+        .min()?;
 
-    println!("{:?}", wire1[0]);
-    println!("{:?}", wire2[0]);
+    println!("{}", closest_to_origin);
+    Some(())
 }
