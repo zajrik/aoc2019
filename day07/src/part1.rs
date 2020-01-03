a7825f02eaff5df49d2804e2788df4ad37e802ed
Zack Campbell <zajrik@gmail.com>
2020-01-03 14:55:33 -0600

    Update day 07 part 1

--- day07/src/part1.rs
+++ day07/src/part1.rs
@@ -1,10 +1,12 @@
 use std::slice::Iter;
 
+// Represents an intcode program instruction parameter mode
 enum Mode {
     Position,
     Immediate,
 }
 
+// Represents an instruction in an intcode program
 struct Instruction {
     op: u8,
     p1: Mode,
@@ -12,6 +14,7 @@ struct Instruction {
 }
 
 impl Instruction {
+    /// Parse an instruction from the given i32
     fn from(instruction: i32) -> Instruction {
         let op: u8 = (instruction % 100) as u8;
         let p1: Mode = Instruction::mode((instruction / 100 % 10) as u8);
@@ -20,6 +23,7 @@ impl Instruction {
         Instruction { op, p1, p2 }
     }
 
+    /// Parse a parameter mode from the given u8
     fn mode(int: u8) -> Mode {
         match int {
             1 => Mode::Immediate,
@@ -27,119 +31,146 @@ impl Instruction {
             _ => panic!("Invalid mode"),
         }
     }
-
-    fn get_value(input: &Vec<i32>, ptr: usize, mode: Mode) -> i32 {
-        match mode {
-            Mode::Position => input[input[ptr] as usize],
-            Mode::Immediate => input[ptr],
-        }
-    }
 }
 
-/// Execute the given program which will consume the given input
-pub fn compute(program: &mut Vec<i32>, input: &Vec<i32>) -> i32 {
-    let mut input_values: Iter<i32> = input.iter();
-    let mut ptr: usize = 0;
-    let mut out: i32 = 0;
+/// Represents an intcode program
+struct Program<'a> {
+    memory: &'a mut Vec<i32>,
+    memptr: usize,
+    input: Iter<'a, i32>,
+    out: i32,
+}
 
-    loop {
-        let instruction: Instruction = Instruction::from(program[ptr]);
+impl<'a> Program<'a> {
+    /// Create a new Program from the given instruction vector and input vector
+    fn new(program: &'a mut Vec<i32>, input: &'a Vec<i32>) -> Program<'a> {
+        Program {
+            memory: program,
+            memptr: 0,
+            input: input.iter(),
+            out: 0,
+        }
+    }
+
+    /// Get the next instruction
+    fn get_instruction(&mut self) -> Instruction {
+        let out: Instruction = Instruction::from(self.memory[self.memptr]);
+        self.memptr += 1;
+        out
+    }
+
+    /// Get next parameters using the given instruction modes
+    fn get_parameters(&mut self, modes: Vec<Mode>) -> Vec<i32> {
+        let out: Vec<i32> = modes
+            .iter()
+            .enumerate()
+            .map(|(i, mode)| match mode {
+                Mode::Position => self.memory[self.memory[self.memptr + i] as usize],
+                Mode::Immediate => self.memory[self.memptr + i],
+            })
+            .collect();
+
+        self.memptr += modes.len();
+        out
+    }
+
+    /// Execute a program cycle. `Some(())` means the program exited
+    fn do_cycle(&mut self) -> Option<()> {
+        let instruction: Instruction = self.get_instruction();
 
         match instruction.op {
             // Add
             1 => {
-                let val1: i32 = Instruction::get_value(&program, ptr + 1, instruction.p1);
-                let val2: i32 = Instruction::get_value(&program, ptr + 2, instruction.p2);
-                let idx: i32 = Instruction::get_value(&program, ptr + 3, Mode::Immediate);
-                program[idx as usize] = val1 + val2;
-                ptr += 4;
+                let modes: Vec<Mode> = vec![instruction.p1, instruction.p2, Mode::Immediate];
+                if let [val1, val2, idx] = self.get_parameters(modes)[..] {
+                    self.memory[idx as usize] = val1 + val2;
+                }
             }
 
             // Multiply
             2 => {
-                let val1: i32 = Instruction::get_value(&program, ptr + 1, instruction.p1);
-                let val2: i32 = Instruction::get_value(&program, ptr + 2, instruction.p2);
-                let idx: i32 = Instruction::get_value(&program, ptr + 3, Mode::Immediate);
-                program[idx as usize] = val1 * val2;
-                ptr += 4;
+                let modes: Vec<Mode> = vec![instruction.p1, instruction.p2, Mode::Immediate];
+                if let [val1, val2, idx] = self.get_parameters(modes)[..] {
+                    self.memory[idx as usize] = val1 * val2;
+                }
             }
 
             // Write input to memory
             3 => {
-                let idx: i32 = Instruction::get_value(&program, ptr + 1, Mode::Immediate);
-                program[idx as usize] = *input_values.next().unwrap() as i32;
-                ptr += 2;
+                if let [idx] = self.get_parameters(vec![Mode::Immediate])[..] {
+                    self.memory[idx as usize] = *self.input.next()?;
+                }
             }
 
             // Set output
             4 => {
-                let val: i32 = Instruction::get_value(&program, ptr + 1, instruction.p1);
-                out = val;
-                ptr += 2;
+                if let [val] = self.get_parameters(vec![instruction.p1])[..] {
+                    self.out = val;
+                }
             }
 
             // Jump if true (non-zero value)
             5 => {
-                let val: i32 = Instruction::get_value(&program, ptr + 1, instruction.p1);
-                let idx: i32 = Instruction::get_value(&program, ptr + 2, instruction.p2);
-
-                if val > 0 {
-                    ptr = idx as usize;
-                } else {
-                    ptr += 3;
+                let modes: Vec<Mode> = vec![instruction.p1, instruction.p2];
+                if let [val, idx] = self.get_parameters(modes)[..] {
+                    if val > 0 {
+                        self.memptr = idx as usize;
+                    }
                 }
             }
 
             // Jump if false (zero value)
             6 => {
-                let val: i32 = Instruction::get_value(&program, ptr + 1, instruction.p1);
-                let idx: i32 = Instruction::get_value(&program, ptr + 2, instruction.p2);
-
-                if val == 0 {
-                    ptr = idx as usize;
-                } else {
-                    ptr += 3;
+                let modes: Vec<Mode> = vec![instruction.p1, instruction.p2];
+                if let [val, idx] = self.get_parameters(modes)[..] {
+                    if val == 0 {
+                        self.memptr = idx as usize;
+                    }
                 }
             }
 
             // Less-than
             7 => {
-                let val1: i32 = Instruction::get_value(&program, ptr + 1, instruction.p1);
-                let val2: i32 = Instruction::get_value(&program, ptr + 2, instruction.p2);
-                let idx: i32 = Instruction::get_value(&program, ptr + 3, Mode::Immediate);
-
-                if val1 < val2 {
-                    program[idx as usize] = 1;
-                } else {
-                    program[idx as usize] = 0;
+                let modes: Vec<Mode> = vec![instruction.p1, instruction.p2, Mode::Immediate];
+                if let [val1, val2, idx] = self.get_parameters(modes)[..] {
+                    if val1 < val2 {
+                        self.memory[idx as usize] = 1;
+                    } else {
+                        self.memory[idx as usize] = 0;
+                    }
                 }
-
-                ptr += 4;
             }
 
             // Equals
             8 => {
-                let val1: i32 = Instruction::get_value(&program, ptr + 1, instruction.p1);
-                let val2: i32 = Instruction::get_value(&program, ptr + 2, instruction.p2);
-                let idx: i32 = Instruction::get_value(&program, ptr + 3, Mode::Immediate);
-
-                if val1 == val2 {
-                    program[idx as usize] = 1;
-                } else {
-                    program[idx as usize] = 0;
+                let modes: Vec<Mode> = vec![instruction.p1, instruction.p2, Mode::Immediate];
+                if let [val1, val2, idx] = self.get_parameters(modes)[..] {
+                    if val1 == val2 {
+                        self.memory[idx as usize] = 1;
+                    } else {
+                        self.memory[idx as usize] = 0;
+                    }
                 }
-
-                ptr += 4;
             }
 
             // Exit
-            99 => break,
+            99 => return Some(()),
 
             _ => panic!("Invalid opcode"),
         }
+
+        None
     }
 
-    out
+    /// Run the program, returning the final output value
+    fn run(&mut self) -> i32 {
+        loop {
+            match self.do_cycle() {
+                Some(()) => return self.out,
+                None => continue,
+            }
+        }
+    }
 }
 
 /// Finds all permutations of the given set of values using Heap's algorithm
@@ -180,12 +211,13 @@ pub fn part1() -> Result<i32, std::io::Error> {
     let phase_setting_sets: Vec<Vec<i32>> = permutations(vec![0, 1, 2, 3, 4]);
     let mut largest_output: i32 = 0;
 
-    for phase_settings in phase_setting_sets {
+    for phase_setting_set in phase_setting_sets {
         let mut last_output: i32 = 0;
 
-        for phase_setting in phase_settings {
-            let result: i32 = compute(&mut input.clone(), &mut vec![phase_setting, last_output]);
-            last_output = result;
+        for phase_setting in phase_setting_set {
+            let mut instructions: Vec<i32> = input.clone();
+            let mut input_set: Vec<i32> = vec![phase_setting, last_output];
+            last_output = Program::new(&mut instructions, &mut input_set).run();
         }
 
         if last_output > largest_output {
