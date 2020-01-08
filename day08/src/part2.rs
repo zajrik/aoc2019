97dd30d134bd4406380634423bafa5ac5b66f712
Zack Campbell <zajrik@gmail.com>
2020-01-08 12:19:45 -0600

    Add day 08 part 2

--- day08/src/part2.rs
+++ day08/src/part2.rs
@@ -1,3 +1,45 @@
-pub fn part2() -> Result<u32, std::io::Err> {
-	Ok(0)
+pub fn part2() -> Result<String, std::io::Error> {
+    let input: &str = include_str!("./input.txt");
+
+    const WIDTH: usize = 25;
+    const HEIGHT: usize = 6;
+    const LAYER_SIZE: usize = HEIGHT * WIDTH;
+
+    let layers: Vec<Vec<char>> = vec![0; input.len() / LAYER_SIZE]
+        .iter()
+        .enumerate()
+        .map(|(i, _)| {
+            input[(LAYER_SIZE * i)..(LAYER_SIZE * (i + 1))]
+                .chars()
+                .collect()
+        })
+        .collect();
+
+    let mut result: Vec<Option<char>> = vec![None; LAYER_SIZE];
+
+    for layer in layers {
+        for (i, pixel) in layer.iter().enumerate() {
+            if result[i].is_none() {
+                result[i] = match *pixel {
+                    '1' => Some('#'),
+                    '0' => Some(' '),
+                    _ => None,
+                }
+            }
+        }
+    }
+
+    let output: String = vec![0; result.len() / WIDTH]
+        .iter()
+        .enumerate()
+        .map(|(i, _)| {
+            result[(WIDTH * i)..(WIDTH * (i + 1))]
+                .iter()
+                .map(|n| n.unwrap_or(' '))
+                .collect::<String>()
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    Ok(format!("\n{}", output))
 }
