7479006c693646dbce524b24f07a63d8920e3f68
Zack Campbell <zajrik@gmail.com>
2020-01-08 12:19:22 -0600

    Update day 08 part 1

--- day08/src/part1.rs
+++ day08/src/part1.rs
@@ -1,28 +1,32 @@
 pub fn part1() -> Result<usize, std::io::Error> {
     let input: &str = include_str!("./input.txt");
-    let layer_size: usize = 25 * 6;
-    let layers: Vec<Vec<u8>> = vec![""; input.len() / layer_size]
+
+    const WIDTH: usize = 25;
+    const HEIGHT: usize = 6;
+    const LAYER_SIZE: usize = HEIGHT * WIDTH;
+
+    let layers: Vec<Vec<char>> = vec![0; input.len() / LAYER_SIZE]
         .iter()
         .enumerate()
         .map(|(i, _)| {
-            input[(layer_size * i)..(layer_size * (i + 1))]
+            input[(LAYER_SIZE * i)..(LAYER_SIZE * (i + 1))]
                 .chars()
-                .map(|c| c.to_string().parse::<u8>().unwrap())
                 .collect()
         })
         .collect();
 
     let mut least_zeroes: (usize, u32) = (0, std::u32::MAX);
 
-    for layer in layers.iter().enumerate() {
-        let zero_count: u32 = layer.1.iter().filter(|n| **n == 0).count() as u32;
+    for (i, layer) in layers.iter().enumerate() {
+        let zero_count: u32 = layer.iter().filter(|n| **n == '0').count() as u32;
+
         if zero_count < least_zeroes.1 {
-            least_zeroes = (layer.0, zero_count);
+            least_zeroes = (i, zero_count);
         }
     }
 
-    let ones: usize = layers[least_zeroes.0].iter().filter(|n| **n == 1).count();
-    let twos: usize = layers[least_zeroes.0].iter().filter(|n| **n == 2).count();
+    let ones: usize = layers[least_zeroes.0].iter().filter(|n| **n == '1').count();
+    let twos: usize = layers[least_zeroes.0].iter().filter(|n| **n == '2').count();
 
     Ok(ones * twos)
 }
